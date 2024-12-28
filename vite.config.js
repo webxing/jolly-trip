eac09e01a81427286ac6be841cbb6aeec2bfb306
webxing <liuxin119@meituan.com>
2024-12-29 00:20:52 +0800

    Update vite base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',  
+  base: '/jolly-trip/',  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
